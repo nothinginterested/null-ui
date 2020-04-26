1c6acaed157c7a0fb8113466508507b30a746c81
你的名字 <>
2020-04-26 15:42:41 +0800

    安装parcel 将button组件改为单文件组件

--- button.js
+++
@@ -1,5 +0,0 @@
-let component = Vue.component('g-button', {
-    template: `
-    <button>按钮</button>
-    `
-});
---
+++ src/button.vue
@@ -0,0 +1,31 @@
+<template>
+    <button class="g-button">按钮</button>
+
+</template>
+<script>
+    export default {
+
+    }
+</script>
+<style lang="scss">
+    .g-button{
+        font-size: var(--font-size);
+        height: var(--button-height);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        border-radius: var(--border-radius);
+        padding: 0 1em;
+        &:hover{
+            border-color: var(--border-color-hover);
+        }
+        &:focus{
+            background: var(--button-active-bg);
+
+        }
+        &:focus{
+            outline: none;
+        }
+
+    }
+
+</style>
