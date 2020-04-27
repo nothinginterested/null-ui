4073e0f0250f3297c5cefe77dc7d6a60602265ee
你的名字 <>
2020-04-27 14:52:29 +0800

    完成了按钮 与 icon-position

--- src/button.vue
+++ src/button.vue
@@ -1,29 +1,64 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg class="icon">
+            <use :xlink:href="`#icon-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 
 </template>
 <script>
     export default {
-
+        props: ["icon", 'iconPosition']
     }
 </script>
 <style lang="scss">
-    .g-button{
+    .g-button {
+        vertical-align: top;
         font-size: var(--font-size);
         height: var(--button-height);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
         border-radius: var(--border-radius);
-        padding: 0 1em;
-        &:hover{
+        padding: 0 1.1em;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+
+        &:hover {
             border-color: var(--border-color-hover);
         }
-        &:focus{
+
+        &:focus {
             background: var(--button-active-bg);
 
         }
-        &:focus{
+
+        &:focus {
             outline: none;
+        }
+
+        > .icon {
+            order: 1;
+            margin-right: .3em;
+        }
+
+        > .content {
+            order: 2;
+        }
+
+        &.icon-right {
+            > .icon {
+                order: 2;
+                margin-left: .3em;
+                margin-right: 0em;
+            }
+
+            > .content {
+                order: 1;
+            }
         }
 
     }
