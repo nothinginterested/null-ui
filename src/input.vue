4129d3220b009c99f6e0f99f5b9cf4f0ddb80e18
你的名字 <>
2020-04-29 03:13:21 +0800

    还缺少对input的单元测试

--- src/input.vue
+++ src/input.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="wrapper" :class="{error}">
-        <input type="text" :value="value" :disabled="disabled" class="{disabled,readonly}" :readonly="readonly">
+        <input type="text" :value="value" :disabled="disabled" class="{disabled,readonly}" :readonly="readonly"
+               @focus="$emit('focus',$event.target.value)"
+               @blur="$emit('blur',$event.target.value)"
+               @change="$emit('change',$event.target.value)"
+               @input="$emit('input',$event.target.value)">
         <template v-if="error">
             <icon name="error" class="icon-error"></icon>
             <span class="errorMessage">{{error}}</span>
