2616554e783cbe6ce6c442a3877b4a7009f4b71c
你的名字 <>
2020-04-29 02:49:52 +0800

    制作基本input样式 加入disabled状态 readonly状态 reset

--- src/app.js
+++ src/app.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
-
+import Input from './input'
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-input', Input)
 new Vue({
     el: '#app',
     data: {
@@ -16,48 +17,4 @@ new Vue({
 })
 
 
-//单元测试
-import chai from 'chai'
-const expect=chai.expect
-
-{
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setting'
-        }
-    }).$mount()
-    const useElement = vm.$el.querySelector('use')
-    expect(useElement.getAttribute('xlink:href')).to.equal('#icon-setting')
-    vm.$destroy()
-}
-
-{
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'setting'
-        }
-    }).$mount()
-    const useElement = vm.$el.querySelector('use')
-    console.log(useElement);
-    console.log(useElement.getAttribute('xlink:href'));
-
-    expect(useElement.getAttribute('xlink:href')).to.equal('#icon-setting')
-    vm.$destroy()
-}
-{
-    const Constructor = Vue.extend(Button)
-    const vm = new Constructor({
-        propsData: {
-            icon: 'settings',
-            loading: true
-        }
-    }).$mount()
-    const useElements = vm.$el.querySelectorAll('use')
-    expect(useElements.length).to.equal(1)
-    console.log(useElements[0]+'ccc');
-    expect(useElements[0].getAttribute('xlink:href')).to.equal('#i-loading')
-    vm.$destroy()
-}
 
---
+++ src/input.vue
@@ -0,0 +1,100 @@
+<template>
+    <div class="wrapper" :class="{error}">
+        <input type="text" :value="value" :disabled="disabled" class="{disabled,readonly}" :readonly="readonly">
+        <template v-if="error">
+            <icon name="error" class="icon-error"></icon>
+            <span class="errorMessage">{{error}}</span>
+        </template>
+    </div>
+</template>
+
+<script>
+    import Icon from './icon'
+
+    export default {
+        name: 'g-input',
+        props: {
+            value: {
+                type: String
+            },
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+            readonly: {
+                type: Boolean,
+                default: false
+            },
+            error: {
+                type: String
+            }
+        },
+        components: {
+            Icon
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    /*--button-height: 32px;*/
+    /*--font-size: 14px;*/
+    /*--button-bg: white;*/
+    /*--border-color: #999;*/
+    /*--border-radius: 4px;*/
+    /*--border-color-hover: #666;*/
+    /*--button-active-bg: #eee;*/
+    $height: 32px;
+    $border-color: #999;
+    $border-color-hover: #666;
+    $border-radius: 4px;
+    $font-size: 14px;
+    $box-shadow-color: rgba(0, 0, 0, 0.5);
+    $red: #F1453D;
+    .wrapper {
+        font-size: $font-size;
+        display: inline-flex;
+        align-items: center;
+
+        > :not(:last-child) {
+            margin-right: .5em;
+        }
+
+        > input {
+            font-size: inherit;
+            height: 32px;
+            padding: 0 8px;
+            border: 1px solid $border-color;
+
+            &:hover {
+                border-color: $border-color-hover
+            }
+
+            border-radius: $border-radius;
+
+            &:focus {
+                box-shadow: inset 0 1px 3px $box-shadow-color;
+                outline: none;
+            }
+
+            &[disabled], &[readonly] {
+                cursor: not-allowed;
+                border-color: #bbb;
+                color: #bbb;
+            }
+        }
+
+        &.error {
+            > input {
+                border-color: $red;
+            }
+        }
+
+        .icon-error {
+            fill: $red;
+        }
+
+        .errorMessage {
+            color: $red;
+        }
+    }
+</style>
