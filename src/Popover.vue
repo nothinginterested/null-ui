c3dd9488030fd1c729a89eb2dfc86a15ecc73ca6
你的名字 <>
2020-05-11 16:08:58 +0800

    四个position的位置搞定

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="Popover" @click="Onclick" ref="Wrapper">
-        <div class="content-wrapper" v-if="Visible" ref="Content">
+        <div class="content-wrapper" v-if="Visible" ref="Content" :class="{[`position-${position}`]:true}">
             <slot name="content"></slot>
         </div>
         <span ref="Trigger" style="display: inline-block">
@@ -21,6 +21,17 @@
                 Visible: false
             }
         },
+        props: {
+            position: {
+                type: String,
+                default: 'top',
+                validator(value) {
+                    return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
+                },
+            }
+
+
+        },
         methods: {
             Onclick(event) {
 
@@ -29,11 +40,28 @@
                     this.Visible = !this.Visible
                     if (this.Visible === true) {
                         this.$nextTick(() => {
-                            let {left, top} = this.$refs.Trigger.getBoundingClientRect()
+                            let {left, top, height, width} = this.$refs.Trigger.getBoundingClientRect()
                             if (this.position === 'top') {
                                 document.body.appendChild(this.$refs.Content)
                                 this.$refs.Content.style.left = left + window.scrollX + 'px'
                                 this.$refs.Content.style.top = top + window.scrollY + 'px'
+                            } else if (this.position === 'bottom') {
+                                document.body.appendChild(this.$refs.Content)
+                                this.$refs.Content.style.left = left + window.scrollX + 'px'
+                                this.$refs.Content.style.top = top + window.scrollY + height + 'px'
+                            } else if (this.position === 'left') {
+                                document.body.appendChild(this.$refs.Content)
+                                let {width, height: height2} = this.$refs.Content.getBoundingClientRect()
+
+                                this.$refs.Content.style.left = left + window.scrollX - width + 'px'
+                                this.$refs.Content.style.top = top + window.scrollY + (height - height2) / 2 + 'px'
+                            } else if (this.position === 'right') {
+                                document.body.appendChild(this.$refs.Content)
+                                let {height: height2} = this.$refs.Content.getBoundingClientRect()
+                                this.$refs.Content.style.left = left + window.scrollX + width + 'px'
+                                this.$refs.Content.style.top = top + window.scrollY + (height - height2) / 2 + 'px'
+
+
                             }
 
 
@@ -78,35 +106,130 @@
 
     .content-wrapper {
         position: absolute;
-        transform: translateY(-100%);
         border: 1px solid $border-color;
         border-radius: $border-radius;
         box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
         padding: .5em 1em;
-        margin-top: -10px;
+        max-width: 20em;
+        word-break: break-all;
 
-        &::before, &::after {
-            content: '';
-            display: block;
-            width: 0px;
-            height: 0px;
-            position: absolute;
-            top: 100%;
-            left: 10px;
+        &.position-top {
+            margin-top: -10px;
+            transform: translateY(-100%);
+
+            &::before, &::after {
+                content: '';
+                display: block;
+                width: 0px;
+                height: 0px;
+                position: absolute;
+                top: 100%;
+                left: 10px;
+
+
+            }
+
+            &::before {
+                border: 10px solid transparent;
+                border-top-color: black;
+            }
+
+            &::after {
+                border: 10px solid transparent;
+                border-top-color: white;
+                top: calc(100% - 1px);
+            }
 
 
         }
 
-        &::before {
-            border: 10px solid transparent;
-            border-top-color: black;
+        &.position-bottom {
+            margin-top: 10px;
+
+            &::before, &::after {
+                content: '';
+                display: block;
+                width: 0px;
+                height: 0px;
+                position: absolute;
+                bottom: 100%;
+                left: 10px;
+
+
+            }
+
+            &::before {
+                border: 10px solid transparent;
+                border-bottom-color: black;
+            }
+
+            &::after {
+                border: 10px solid transparent;
+                border-bottom-color: white;
+                bottom: calc(100% - 1px);
+            }
+
+
         }
 
-        &::after {
-            border: 10px solid transparent;
-            border-top-color: white;
-            top: calc(100% - 1px);
+        &.position-left {
+            margin-left: -10px;
+
+            &::before, &::after {
+                content: '';
+                display: block;
+                width: 0px;
+                height: 0px;
+                position: absolute;
+                left: 100%;
+                transform: translateY(-50%);
+                top: 50%
+
+            }
+
+            &::before {
+                border: 10px solid transparent;
+                border-left-color: black;
+            }
+
+            &::after {
+                border: 10px solid transparent;
+                border-left-color: white;
+                left: calc(100% - 1px);
+            }
+
+
         }
+
+        &.position-right {
+            margin-left: 10px;
+
+            &::before, &::after {
+                content: '';
+                display: block;
+                width: 0px;
+                height: 0px;
+                position: absolute;
+                right: 100%;
+                transform: translateY(-50%);
+                top: 50%
+
+            }
+
+            &::before {
+                border: 10px solid transparent;
+                border-right-color: black;
+            }
+
+            &::after {
+                border: 10px solid transparent;
+                border-right-color: white;
+                right: calc(100% - 1px);
+            }
+
+
+        }
+
 
     }
 
